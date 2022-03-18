096541451bab1e689afa9a65095c6d9e03f4d1e3
Manavendra Sen <manavendra4288@gmail.com>
2022-03-19 04:46:15 +0530

    feat: setup complete

--- pages/project/[slug].tsx
+++ pages/project/[slug].tsx
@@ -6,6 +6,7 @@ import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 import { getPost } from "../../api/postRequests";
 
 import styles from "../../styles/Page/Project.module.scss";
+import Head from "next/head";
 
 interface IProps {
   post: any;
@@ -20,6 +21,11 @@ const Project: React.FC<IProps> = ({ post }) => {
 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>{post.title}</title>
+        <meta name='description' content={`${post.custom_excerpt}`} />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <Link href='/'>
         <a>Home</a>
       </Link>
