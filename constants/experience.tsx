cb2869e6d2bb63f68dd441d77b9054e7bb2a3694
Manavendra Sen <manavendra4288@gmail.com>
2022-04-20 20:36:15 +0530

    feat: experience section

--- constants/experience.tsx
+++ constants/experience.tsx
@@ -14,73 +14,68 @@ export interface ExperienceInterface {
 const experience: ExperienceInterface[] = [
   {
     id: 1,
-    title: "Software Engineering Intern",
-    logo: "https://via.placeholder.com/200",
-    company: "Google",
-    location: "Mountain View, CA",
-    startDate: "May 2019",
-    endDate: "Aug 2019",
+    title: "Full Stack Developer Intern (Remote)",
+    logo: "https://user-images.githubusercontent.com/26283488/164260309-6dd1a59e-aa8d-4a41-9f29-a7a97b77ff9e.png",
+    company: "416 Marketing Inc",
+    location: "Toronto, Canada",
+    startDate: "Jan 2022",
+    endDate: "March 2022",
     description: (
-      <ul>
+      <ul className="flex flex-col gap-4">
         <li>
-          Ad sint commodo eiusmod pariatur eiusmod nulla ullamco irure duis
-          dolor qui quis duis.
+          Involved in project plan meetings and requirement documentation with
+          clients, business analysts, and team members to analyze business
+          requirements and translated business requirements into the working
+          model using Agile methodology.
         </li>
-        <li>Tempor nostrud enim cupidatat veniam nulla et.</li>
+        <li>
+          Worked as a part of the development on UI and Frontend as well as Node
+          Rest APIs, used PostgreSQL and TypeORM for database services.
+        </li>
       </ul>
     ),
   },
   {
     id: 2,
-    title: "Software Engineering Intern",
-    logo: "https://via.placeholder.com/200",
-    company: "Google",
-    location: "Mountain View, CA",
-    startDate: "May 2019",
-    endDate: "Aug 2019",
+    title: "Software Developer Engineer Intern (Remote)",
+    logo: "https://user-images.githubusercontent.com/26283488/164258838-3cb0f616-66c2-40a1-b87b-9df983051c53.jpeg",
+    company: "Listnr, Inc",
+    location: "Middletown, Delaware, USA",
+    startDate: "Jul 2021",
+    endDate: "Sept 2021",
     description: (
-      <ul>
+      <ul className="flex flex-col gap-4">
         <li>
-          Ad sint commodo eiusmod pariatur eiusmod nulla ullamco irure duis
-          dolor qui quis duis.
+          Implementing user-facing features for a global audience that allows
+          users to import their podcasts from services like Spotify to Listnr
+          and create custom web pages for their podcasts.
         </li>
-        <li>Tempor nostrud enim cupidatat veniam nulla et.</li>
+        <li>
+          Coordinated with other developers to set up Sentry and configured
+          different environments for testing, production etc, which helped the
+          team to identify and resolve bugs 70% faster.
+        </li>
       </ul>
     ),
   },
   {
     id: 3,
-    title: "Software Engineering Intern",
-    logo: "https://via.placeholder.com/200",
-    company: "Google",
-    location: "Mountain View, CA",
-    startDate: "May 2019",
-    endDate: "Aug 2019",
+    title: "Full Stack Web Development Intern (Remote)",
+    logo: "https://user-images.githubusercontent.com/26283488/164261907-efd648f1-2f6e-47a3-b8e4-e47194a016ce.png",
+    company: "DESIGN IN SITU",
+    location: "Kadıköy, İstanbul, Turkey",
+    startDate: "June 2021",
+    endDate: "Aug 2021",
     description: (
-      <ul>
+      <ul className="flex flex-col gap-4">
         <li>
-          Ad sint commodo eiusmod pariatur eiusmod nulla ullamco irure duis
-          dolor qui quis duis.
+          Collaborated with international clients and helped the team in
+          translating business and customer interests into software solutions.
         </li>
-        <li>Tempor nostrud enim cupidatat veniam nulla et.</li>
-      </ul>
-    ),
-  },
-  {
-    id: 4,
-    title: "Software Engineering Intern",
-    logo: "https://via.placeholder.com/200",
-    company: "Google",
-    location: "Mountain View, CA",
-    startDate: "May 2019",
-    endDate: "Aug 2019",
-    description: (
-      <ul>
         <li>
-          Ad sint commodo eiusmod pariatur eiusmod nulla ullamco irure duis
-          dolor qui quis duis.
+          Responsible for development of music generation applications using
+          React.js, Tone.js, and Firebase, accessed by 1000+ users worldwide.
         </li>
-        <li>Tempor nostrud enim cupidatat veniam nulla et.</li>
       </ul>
     ),
   },
