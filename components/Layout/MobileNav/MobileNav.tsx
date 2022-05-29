86472932edfc78d3275f5ab10cb26976151a9ec9
Manavendra Sen <manavendra4288@gmail.com>
2022-05-30 00:58:23 +0530

    feat: remove ununsed code

--- components/Layout/MobileNav/MobileNav.tsx
+++ components/Layout/MobileNav/MobileNav.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import navLinks from "../../../constants/navLinks";
+// import navLinks from "../../../constants/navLinks";
 
 interface MobileNavProps {}
 
@@ -8,13 +8,13 @@ const MobileNav: React.FC<MobileNavProps> = () => {
   return (
     <div className="z-50 fixed bottom-16 bg-slate-50 rounded-3xl py-2 pl-6 shadow-md left-1/2 -translate-x-1/2 block xl:hidden">
       <div className="flex justify-center items-center gap-5 w-max">
-        {navLinks.map(link => (
+        {/* {navLinks.map(link => (
           <div key={link.name}>
             <Link href={link.href}>
               <a>{link.name}</a>
             </Link>
           </div>
-        ))}
+        ))} */}
         <div>
           <Link href="/">
             <a className="bg-blue-600 hover:bg-blue-700 transition-all px-3 py-2 text-white font-medium rounded-3xl">
