4d5d2bb0f365fb627a04af333cab3c539e22323c
Manavendra Sen <manavendra4288@gmail.com>
2022-06-01 02:22:57 +0530

    chore: cleanup unused code

--- components/Layout/MobileNav/MobileNav.tsx
+++ components/Layout/MobileNav/MobileNav.tsx
@@ -8,13 +8,6 @@ const MobileNav: React.FC<MobileNavProps> = () => {
   return (
     <div className="z-50 fixed bottom-16 bg-slate-50 rounded-3xl py-2 pl-6 shadow-md left-1/2 -translate-x-1/2 block xl:hidden">
       <div className="flex justify-center items-center gap-5 w-max">
-        {/* {navLinks.map(link => (
-          <div key={link.name}>
-            <Link href={link.href}>
-              <a>{link.name}</a>
-            </Link>
-          </div>
-        ))} */}
         <div>
           <Link href="/">
             <a className="bg-blue-600 hover:bg-blue-700 transition-all px-3 py-2 text-white font-medium rounded-3xl">
