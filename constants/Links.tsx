4081c7767ee3f5f82fa10fe136b6115c1f85c8b1
Manavendra Sen <manavendra4288@gmail.com>
2022-05-31 17:54:06 +0530

    feat: add social links

---
+++ constants/Links.tsx
@@ -0,0 +1,51 @@
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaBehance } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { SiDevpost } from "react-icons/si";
+
+export const Links = [
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/manavendrasen/",
+    icon: <FaLinkedinIn />,
+    primaryColor: "#0077B5",
+    color: "#ffffff",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/manavendrasen",
+    icon: <FaGithub />,
+    primaryColor: "#23272D",
+    color: "#ffffff",
+  },
+  {
+    name: "Peerlist",
+    href: "https://peerlist.io/manavendrasen",
+    icon: <SiDevpost />,
+    primaryColor: "#00AA45",
+    color: "#ffffff",
+  },
+  {
+    name: "Behance",
+    href: "https://www.behance.net/manavendrasen",
+    icon: <FaBehance />,
+    primaryColor: "#0057FF",
+    color: "#ffffff",
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/channel/UCEcCKqet47IWQFRt9zfGw8Q",
+    icon: <FaYoutube />,
+    primaryColor: "#FF0000",
+    color: "#ffffff",
+  },
+  {
+    name: "Devpost",
+    href: "https://devpost.com/manavendrasen",
+    icon: <SiDevpost />,
+    primaryColor: "#00394E",
+    color: "#ffffff",
+  },
+  {},
+];
