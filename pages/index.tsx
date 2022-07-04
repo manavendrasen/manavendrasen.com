425d54101daefb466bb1615c083e0b6d6024e93f
Manavendra Sen <manavendra4288@gmail.com>
2022-07-04 18:44:29 +0530

    Revert "feat: add theme random" + Build: add mui
    This reverts commit 8c754c6ecca738d24b0ed4e0fc43efa75882d102.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,5 @@
-import Link from "next/link";
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
-import Footer from "../components/Layout/Footer/Footer";
-import Container from "../components/Layout/Container/Container";
-import Navbar from "../components/Layout/Navbar/Navbar";
 import { Head } from "../components/Head/Head";
-import MobileNav from "../components/Layout/MobileNav/MobileNav";
-import Select from "react-select";
 import { THEME } from "../constants/Theme";
 
 import { ThemeHandler } from "../components/Layout/ThemeHandler/ThemeHandler";
@@ -14,36 +7,13 @@ import { ThemeHandler } from "../components/Layout/ThemeHandler/ThemeHandler";
 interface IHomeProps {}
 
 const Home: NextPage<IHomeProps> = () => {
-  const options = [
-    { value: THEME.LIGHT, label: "Light" },
-    { value: THEME.DARK, label: "Dark" },
-    { value: THEME.GRUVBOX, label: "Gruvbox" },
-  ];
-
-  const [theme, setTheme] = useState(options[0]);
-
   return (
     <div className="relative">
       <Head
         title="Portfolio - Manavendra Sen | Home"
         description="A blog about software development, programming, and more."
       />
-      <nav className="absolute left-0 top-0 w-full">
-        <Container>
-          <div className="flex justify-end items-end py-8">
-            <div>
-              <Select
-                defaultValue={theme}
-                onChange={opt => {
-                  setTheme(opt!);
-                }}
-                options={options}
-              />
-            </div>
-          </div>
-        </Container>
-      </nav>
-      <ThemeHandler theme={theme.value} />
+      <ThemeHandler theme={THEME.DARK} />
     </div>
   );
 };
