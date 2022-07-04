442fc578531fb85a6ffbb0f4e33b5c2dd85db284
Manavendra Sen <manavendra4288@gmail.com>
2022-07-04 23:33:44 +0530

    fix: component jsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,12 @@ import type { AppProps } from "next/app";
 import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />;
+    </React.Fragment>
+  );
+
 }
 
 export default MyApp;
