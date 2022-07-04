8ef091cf793310e0df29e46c3b781164edf8b1df
Manavendra Sen <manavendra4288@gmail.com>
2022-07-04 18:31:31 +0530

    feat: STABLE

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import React from "react";
 
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
 
 // 1. Import the extendTheme function
-import { extendTheme } from "@chakra-ui/react";
+// import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
   brand: {
@@ -23,12 +23,12 @@ const colors = {
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const theme = extendTheme({ colors });
+  // const theme = extendTheme({ colors });
 
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    // <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+    // </ChakraProvider>
   );
 }
 
