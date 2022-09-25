9109bd5bca70bb6d61a04b75a4742b629ba40044
Manavendra Sen <manavendra4288@gmail.com>
2022-09-26 02:01:23 +0530

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+
+// <script async src=""></script>
+// <script>
+//
+// </script>
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+      />
+      <Script strategy="lazyOnload" id="gtag">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${process.env.NEXT_PUBLIC_ANALYTICS_ID});`}
+      </Script>
       <Component {...pageProps} />
     </React.Fragment>
   );
